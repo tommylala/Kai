2c4eacaac057acd133cc9f365973dbc6cd67ff12
tommylala <30609221+tommylala@users.noreply.github.com>
2017-09-25 18:34:03 -0400

    Project Update 2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-ddd
+$(".hamburger").click(function() {
+  $(this).hide();
+  $('.cross').show();
+  $(".menu").slideToggle();
+});
+$(".cross").click(function() {
+  $(this).hide();
+  $('.hamburger').show();
+  $(".menu").slideToggle();
+});
